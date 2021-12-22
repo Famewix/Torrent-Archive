89de1ce440b9d6847cca89e2a7af637b718a95b5
Faridin Islam <faridin05saif@gmail.com>
2021-12-22 18:55:08 +0600

    Completed index page.

--- website/views.py
+++ website/views.py
@@ -2,13 +2,46 @@ from flask import Blueprint, render_template, url_for
 import datetime
 import os
 
+posts = [
+    {
+        'title': 'Torrent 1',
+        'magnet': 'kaljnskndhnakishdihaiushihdaishdihaishdihaishdahsdihahsihaihsdhasidhiahidhiashdia',
+        'category': 'movie',
+        'posted': '26 March, 2021'
+    },
+    {
+        'title': 'Torrent 2',
+        'magnet': 'kaljnskndhnakishdihaiushihdaishdihaishdihaishdahsdihahsihaihsdhasidhiahidhiashdia',
+        'category': 'movie',
+        'posted': '26 March, 2021'
+    },
+    {
+        'title': 'Torrent 3',
+        'magnet': 'llllaljnskndhnakishdihaiushihdaishdihaishdihaishdahsdihahsihaihsdhasidhiahidhiashdia',
+        'category': 'movie',
+        'posted': '26 March, 2021'
+    },
+    {
+        'title': 'Torrent 4',
+        'magnet': 'awwwaljnskndhnakishdihaiushihdaishdihaishdihaishdahsdihahsihaihsdhasidhiahidhiashdia',
+        'category': 'movie',
+        'posted': '26 March, 2021'
+    },
+    {
+        'title': 'Torrent 5',
+        'magnet': 'kaljnskndhnakishdihaiushihdaishdihaishdihaishdahsdihahsihaihsdhasidhiahidhiashdia',
+        'category': 'movie',
+        'posted': '26 March, 2021'
+    },
+]
+
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 
 @views.route("/about")
